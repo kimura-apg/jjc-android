5972bd2707295eca78aaccdeeb3e07fcbfa5d106
kimura-apg <kimura@arcplg.com>
2026-05-14 12:22:27 +0900

    temp

--- app/src/main/java/com/arcplg/myapplication/MainActivity.java
+++ app/src/main/java/com/arcplg/myapplication/MainActivity.java
@@ -41,13 +41,11 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action!!!!!!!", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+                NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
             }
         });
     }
-    
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
